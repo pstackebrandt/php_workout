0b6830b12a69d2ee561d23ef2d51ff6e83211931
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-18 17:53:26 +0200

    Add template

---
+++ _template.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1); ?>
+<!doctype html>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>workout</title>
+</head>
+
+<body>
+	<h1>Workout</h1>
+
+	<?php $test = 'test';
+	echo "<h1>Test</h1>\n";
+	echo "<p>\$test = $test;</p>\n";
+	?>
+
+</body>
+
+</html>

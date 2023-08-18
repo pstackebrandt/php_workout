5a8da15396119bbbd9743ec08a73a5be341977f0
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-18 17:54:13 +0200

    template: reformat

--- _template.php
+++ _template.php
@@ -1,21 +1,23 @@
-<?php declare(strict_types=1); ?>
+<?php
+
+declare(strict_types=1); ?>
 <!doctype html>
 
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>workout</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>workout</title>
 </head>
 
 <body>
-	<h1>Workout</h1>
+   <h1>Workout</h1>
 
-	<?php $test = 'test';
-	echo "<h1>Test</h1>\n";
-	echo "<p>\$test = $test;</p>\n";
-	?>
+   <?php $test = 'test';
+   echo "<h1>Test</h1>\n";
+   echo "<p>\$test = $test;</p>\n";
+   ?>
 
 </body>
 

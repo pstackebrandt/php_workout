3670fe3e69075c92eb08e8f6d6ffd8db2f940b63
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-23 14:59:38 +0200

    works: Add simple page content

---
+++ works_prepared_statements/i_part1_create_works_form.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../utility/print_helper.php';
+?>
+<!doctype html>
+
+<html>
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>works</title>
+</head>
+
+<body>
+   <h1>Create page with form</h1>
+
+   <?php
+
+
+   ?>
+
+</body>
+
+</html>

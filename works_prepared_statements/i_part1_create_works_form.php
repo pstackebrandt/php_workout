1583966edbef63e0a31a1cb99235f5094c6af732
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-23 15:18:54 +0200

    works: Add default file from teacher

--- works_prepared_statements/i_part1_create_works_form.php
+++ works_prepared_statements/i_part1_create_works_form.php
@@ -3,6 +3,11 @@
 declare(strict_types=1);
 
 require_once '../utility/print_helper.php';
+
+require_once('./include/config.inc.php');
+require_once('./include/db.inc.php');
+require_once('./include/form.inc.php');
+
 ?>
 <!doctype html>
 
@@ -12,6 +17,9 @@ require_once '../utility/print_helper.php';
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>works</title>
+
+   <link rel="stylesheet" href="./css/main.css">
+   <link rel="stylesheet" href="./css/debug.css">
 </head>
 
 <body>
---
+++ works_prepared_statements/include/config.inc.php
@@ -0,0 +1,56 @@
+<?php
+#****************************************************************************************************#
+				
+				
+				#***************************************************#
+				#********** GLOBALE PROJECT CONFIGURATION **********#
+				#***************************************************#
+				
+				/*
+					Konstanten werden in PHP mittels der Funktion define() oder über 
+					das Schlüsselwort const (const DEBUG = true;) definiert. 
+					Konstanten besitzen im Gegensatz zu Variablen kein $-Präfix
+					Üblicherweise werden Konstanten komplett GROSS geschrieben.
+					
+					1. Verwendung von const:
+					Mit dem const-Schlüsselwort können Konstanten innerhalb von Klassen definiert werden.
+					Die Verwendung von const ist auf Klassenebene beschränkt und erfordert, dass die Konstante in 
+					einer Klasse definiert wird.
+					Konstanten, die mit const definiert werden, sind implizit öffentlich (public) und können direkt
+					über den Klassennamen aufgerufen werden, ohne eine Instanz der Klasse zu erstellen.
+					Beispiel: const LOGIN_TYPE = email;			$loginType = User::LOGIN_TYPE;
+					
+					2. Verwendung von define():
+					Die Funktion define() wird außerhalb von Klassen verwendet, um Konstanten zu definieren.
+					define() kann Konstanten global in jedem Bereich des Codes definieren.
+					Konstanten, die mit define() definiert werden, sind standardmäßig global und können überall 
+					im Code verwendet werden.
+					Beispiel: define('DEBUG', true);
+				*/
+				
+				
+				#********** DATABASE CONFIGURATION **********#
+				define('DB_SYSTEM',							'mysql');
+				define('DB_HOST',								'localhost');
+				define('DB_NAME',								'market');
+				define('DB_USER',								'root');
+				define('DB_PWD',								'');
+				
+				
+				#********** EXTERNAL INPUT STRING CONFIGURATION **********#
+				define('INPUT_MAX_LENGTH',	256);
+				define('INPUT_MIN_LENGTH',	0);
+				
+				
+				#********** DEBUGGING **********#
+				define('DEBUG', 				true);				// Debugging for main document
+				// define('DEBUG', 				false);				// Debugging for main document
+				// define('DEBUG_V', 			true);				// Debugging for values
+				define('DEBUG_V', 			false);				// Debugging for values
+				// define('DEBUG_F', 			true);				// Debugging for functions
+				define('DEBUG_F', 			false);				// Debugging for functions
+				// define('DEBUG_DB', 			true);				// Debugging for DB operations
+				define('DEBUG_DB', 			false);				// Debugging for DB operations
+
+
+#****************************************************************************************************#

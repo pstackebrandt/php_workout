f3dafc3cde38355e858bfde79e958525e6790f4a
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-23 15:32:09 +0200

    works: Add worksForm with author, title, price and submit

--- works_prepared_statements/i_part1_create_works_form.php
+++ works_prepared_statements/i_part1_create_works_form.php
@@ -20,16 +20,38 @@ require_once('./include/form.inc.php');
 
    <link rel="stylesheet" href="./css/main.css">
    <link rel="stylesheet" href="./css/debug.css">
+   <link rel="stylesheet" href="./css/project_specific.css">
 </head>
 
 <body>
-   <h1>Create page with form</h1>
+   <h1>Create works page with form</h1>
 
    <?php
 
-
    ?>
 
+   <h2>Werke-Erfassung</h>
+
+   <br>
+
+   <form action="" method="POST">
+      <input type="hidden" name="worksForm">
+      <br>
+
+      <label>Autor</label><br>
+      <input type="text" name="author" value="Peter"><br>
+      <br>
+
+      <label>Titel</label><br>
+      <input type="text" name="title" value="Wilde Geschichten"><br>
+      <br>
+
+      <label>Preis</label><br>
+      <input type="text" name="price" value="29.95"><br>
+      <br>
+
+      <input type="submit" value="Werk speichern">
+   </form>
 </body>
 
 </html>

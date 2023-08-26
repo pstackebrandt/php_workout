1c7f5c6e6508ba938fadc0ddcd806866b19d663e
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-08-26 11:31:57 +0200

    log_to_logfile_in_application.php: log to html log

--- log_to_logfile_in_application.php
+++ log_to_logfile_in_application.php
@@ -15,17 +15,37 @@ declare(strict_types=1); ?>
    <h1>Workout</h1>
 
    <?php
-   echo "<h1>Log to logfile</h1>\n";
-   
-   // Bestimmen Sie den Pfad zu Ihrem Log-File im Anwendungsordner
-   $logFile = __DIR__ . '/mein-logfile.log';
+   echo "<h1>Log to html logfile</h1>\n";
 
-   $e = new Exception('Test Exception');
-   
+   // Bestimmen Sie den Pfad zu Ihrem Log-File im Anwendungsordner
+   // $logFile = __DIR__ . '/mein-logfile.log';
+
+   $e = new Exception('Could not connect to the starship enterprise');
+
    // Verwenden Sie error_log(), um in die spezifizierte Datei zu schreiben
-   error_log("Database Error: " . $e->getMessage(), 3, $logFile);
+   // error_log("Database Error: " . $e->getMessage(), 3, $logFile);
+
+   // Ordner erzeugen
+   @mkdir('./logfiles');
+
+   // $errorMessage = 'Ungültiger Login - Password falsch';
+   $errorMessage = $e->getMessage();
+
+   // Vorbereiten des Logeintrags
+   $logEntry  = "<p>";
+   $logEntry .= date('Y-m-d | H:i:s') . " | ";
+   $logEntry .= "File: <i>'" . __FILE__ . "'</i> | ";
+   $logEntry .= "<b>$errorMessage</b>";
+   $logEntry .= "</p>\n";
+
+   // Vorbereiteten LogEintrag in 'errorLog.html' speichern
+   file_put_contents('./logfiles/errorLog.html', $logEntry, FILE_APPEND);
    ?>
 
 </body>
+
+<h3>Error Log:</h3>
+
+<?php include('./logfiles/errorLog.html') ?>
 
 </html>
